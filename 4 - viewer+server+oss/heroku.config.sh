78d85df7d6dd80808ea4ac7f35b719d59b00f692
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-09-26 10:03:32 +0200

    removed boilerplate samples 1 to 5

--- 4 - viewer+server+oss/heroku.config.sh
+++
@@ -1,5 +0,0 @@
-heroku.config.sh#!/bin/sh
-
-heroku config:set NODE_ENV=production
-heroku config:set FORGE_CLIENTID=$FORGE_CLIENT_ID
-heroku config:set FORGE_CLIENTSECRET=$FORGE_CLIENT_SECRET
--- 5 - viewer+server+oss+derivatives/bin/run.js
+++
@@ -1,8 +0,0 @@
-
-require('babel-core/register')({
-  presets: ['es2015-node5', 'stage-0']
-});
-
-require('babel-polyfill');
-
-require('../src/server/server');
